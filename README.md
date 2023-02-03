d1a7da07c1cb8b49c5fbb006dc1aeb99d8404e47
macrografi <macrografi@hotmail.com>
2023-02-03 19:17:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-operators
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-mrfdkf)
---
+++ index.ts
@@ -0,0 +1,18 @@
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+//emit ({name: 'Joe', age: 30}, {name: 'Frank', age: 20},{name: 'Ryan', age: 50})
+const source = from([
+  { name: 'Joe', age: 30 },
+  { name: 'Frank', age: 20 },
+  { name: 'Ryan', age: 50 },
+]);
+//grab each persons name, could also use pluck for this scenario
+const example = source.pipe(
+  map(({ name, age }) => {
+    name;
+    return age + 5;
+  })
+);
+//output: "Joe","Frank","Ryan"
+const subscribe = example.subscribe((val) => console.log(val));
